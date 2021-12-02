d4d3973abd134219c09794a265239cdceef02343
hatboan <53263563+hatboan@users.noreply.github.com>
2021-12-02 23:17:06 +0900

    Update 1181.cpp

--- 1181.cpp
+++ 1181.cpp
@@ -1,47 +1,77 @@
 #include <iostream>
-#include <cstring>
 #include <string>
+#include <cstring>
 using namespace std;
+char sorted[30000][60];
+
+void merge(char arr[30000][60],int left, int mid, int right){
+    int i,j,k,l;
+    i=left;
+    j=mid+1;
+    k=left;
+    
+    while(i<=mid && j<=right){
+        if(strlen(arr[i])<strlen(arr[j]))
+            strcpy(sorted[k++],arr[i++]);
+        else if(strlen(arr[i])>strlen(arr[j]))
+            strcpy(sorted[k++],arr[j++]);
+        else{
+            string stri(arr[i]);
+            string strj(arr[j]);
+            if(stri.compare(strj)<0)
+                strcpy(sorted[k++],arr[i++]);
+            else
+                strcpy(sorted[k++],arr[j++]);
+        }
+    }
+
+    if(i>mid){
+        for(l=j;l<=right;l++)
+            strcpy(sorted[k++],arr[l]);
+    }
+
+    else{
+        for(l=i;l<=mid;l++)
+            strcpy(sorted[k++],arr[l]);
+    }
+
+    for(l=left;l<=right;l++)
+        strcpy(arr[l],sorted[l]);
+}
+
+void mergesort(char arr[30000][60],int left,int right){
+    if(left<right){
+        int mid=(left+right)/2;
+        mergesort(arr,left,mid);
+        mergesort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+}
 
 int main(){
     int n;
-    char incha[25001][60];
-    char outcha[25001][60];
+    char cha[30000][60];
     cin>>n;
-    for (int i=0;i<n;i++){
+    for(int i=0;i<n;i++){
         cin>>cha[i];
     }
-    for (int i=0;i<n;i++){
-        int min=i;
-        for(int j=i;j<n;j++){
-            if(strlen(cha[min])>strlen(cha[j]))
-                min=j;
-            else if(strlen(cha[min])==strlen(cha[j])){
-                if(strcmp(cha[min],cha[j])==0){
-                    min=j;
-                }
-                else{
-                    string strmin(cha[min]);
-                    string strj(cha[j]);
-                    if(strmin.compare(strj)>0){
-                        min=j;
-                    }
-                }
-            }
-        }
-        strcpy(cha[20000],cha[min]);
-        strcpy(cha[min],cha[i]);
-        strcpy(cha[i],cha[20000]);
-    }
-    for(int i=0;i<n-1;i++){
-        if(strcmp(cha[i],cha[i+1])==0){
-            for(int j=i;j<n-1;j++)
-                strcpy(cha[j],cha[j+1]);
-            n--;
-        }
-    }
-    for (int i=0;i<n;i++){
+    mergesort(cha,0,n-1);
+
+	for(int i=0; i < n-1; i++){ 
+		if(cha[i] == cha[i+1]){
+			for(int j=i; j < n-1; j++){
+				strcpy(cha[j],cha[j+1]);
+			}
+			n--;
+		}
+	}
+
+    if(cha[0] == cha[1] && n != 1) {
+		for(int i=1; i < n-1; i++)	strcpy(cha[i],cha[i+1]);
+		n--;
+	}
+
+    for(int i=0;i<n;i++)
         cout<<cha[i]<<endl;
-    }
     return 0;
 }
