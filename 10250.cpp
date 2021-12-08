c31c12e8ab576e5444d5a6e08f7263cee179b983
hatboan <53263563+hatboan@users.noreply.github.com>
2021-12-08 23:18:04 +0900

    Create 10250.cpp

---
+++ 10250.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t,h,w,n,i;
+    cin>>t;
+    for(i=0;i<t;i++){
+        cin>>h>>w>>n;
+        if(n%h==0)
+            cout<<h<<(n/h<9?"0":"")<<n/h<<"\n";
+        else
+            cout<<n%h<<(n/h<9?"0":"")<<n/h+1<<"\n";
+    }
+}
