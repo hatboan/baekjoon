3ae541da77d38635fed64fbdff0eeef6dcda882a
hatboan <53263563+hatboan@users.noreply.github.com>
2021-12-12 22:13:30 +0900

    Update 10250.cpp

--- 10250.cpp
+++ 10250.cpp
@@ -7,8 +7,9 @@ int main(){
     for(i=0;i<t;i++){
         cin>>h>>w>>n;
         if(n%h==0)
-            cout<<h<<(n/h<9?"0":"")<<n/h<<"\n";
+            cout<<h<<(n/h<=9?"0":"")<<n/h<<"\n";
         else
-            cout<<n%h<<(n/h<9?"0":"")<<n/h+1<<"\n";
+            cout<<n%h<<(n/h+1<=9?"0":"")<<n/h+1<<"\n";
     }
+    return 0;
 }
