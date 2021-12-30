8e54f0e60ba092693de9c7d0ea5c96f7c1c5d9f7
hatboan <53263563+hatboan@users.noreply.github.com>
2021-12-30 22:24:06 +0900

    Update 1074.cpp
    시간 문제를 해결하기 위해 target_x와 target_y가 범위 내에 있을 때만 재귀함수를 돌렸고, 제곱표현은 <<가 아닌 <cmath> 라이브러리의 pow를 가져다 썼더니 해결되었다.

--- 1074.cpp
+++ 1074.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int now=0;
 
@@ -23,15 +24,27 @@ void z(int x,int y,int tar_x,int tar_y,int divided){
   }
   else{
     int newdiv=divided/2;
-    z(x,y,tar_x,tar_y,newdiv);
-    z(x+newdiv,y,tar_x,tar_y,newdiv);
-    z(x,y+newdiv,tar_x,tar_y,newdiv);
-    z(x+newdiv,y+newdiv,tar_x,tar_y,newdiv);
+    if(x<=tar_x&&tar_x<x+newdiv&&y<=tar_y&&tar_y<y+newdiv)
+        z(x,y,tar_x,tar_y,newdiv);
+    else
+        now+=pow(newdiv,2);
+    if(x<=tar_x+newdiv&&tar_x<x+newdiv+newdiv&&y<=tar_y&&tar_y<y+newdiv)
+        z(x+newdiv,y,tar_x,tar_y,newdiv);
+    else
+        now+=pow(newdiv,2);
+    if(x<=tar_x&&tar_x<x+newdiv&&y<=tar_y+newdiv&&tar_y<y+newdiv+newdiv)
+        z(x,y+newdiv,tar_x,tar_y,newdiv);
+    else
+        now+=pow(newdiv,2);
+    if(x<=tar_x+newdiv&&tar_x<x+newdiv+newdiv&&y<=tar_y+newdiv&&tar_y<y+newdiv+newdiv)
+        z(x+newdiv,y+newdiv,tar_x,tar_y,newdiv);
+    else
+        now+=pow(newdiv,2);
   }
 }
 
 int main(){
-  int n,r,c,l,k;
+  int n,r,c;
   scanf("%d %d %d",&n,&r,&c);
   z(0,0,c,r,2<<n);
 }
