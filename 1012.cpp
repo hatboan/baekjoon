a7709c2e2b6fc8c4cba0f5427d72269bc2e9450c
hatboan <53263563+hatboan@users.noreply.github.com>
2022-01-08 22:45:04 +0900

    Update 1012.cpp
    드디어 dfs를 성공해서 문제를 풀었다. 상당히 스릴 넘친다. 그래프 위주로 문제를 계속 풀어나갈 생각이다.

--- 1012.cpp
+++ 1012.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-bool visited[52][52];
-int graph[51][51];
 
-void dfs(int x,int y){
-    visited[x][y]=true;
-    printf("x:%d y:%d\n",x,y);
-    if(graph[x-1][y]==1&&visited[x-1][y]==false)
-        dfs(x-1,y);
-    if(graph[x][y+1]==1&&visited[x][y+1]==false)
-        dfs(x,y+1);   
-    if(graph[x+1][y]==1&&visited[x+1][y]==false)
-        dfs(x+1,y);
-    if(graph[x][y-1]==1&&visited[x][y-1]==false)
-        dfs(x,y-1);    
+#define MAX 2500
+int land [50][50];
+int visit[MAX]={0,};
+int M,N,K;
+
+void dfs(int v){
+    visit[v]=1;
+    int x[4]={1,-1,0,0};
+    int y[4]={0,0,1,-1};
+    for(int i=0;i<4;i++){
+        int nx=v%M+x[i];
+        int ny=v/M+y[i];
+        if(nx>=0&&nx<M&&ny>=0&&ny<N){
+            if(land[nx][ny]==1&&visit[ny*M+nx]==0){
+                dfs(ny*M+nx);
+            }
+        }
+    }
 }
 
 int main(){
-    int t,m,n,k,x,y;
+    int t,l;
     scanf("%d",&t);
     while(t--){
-        scanf("%d %d %d",&m,&n,&k);
-        while(k--){
-            scanf("%d %d",&x,&y);
-            graph[x+1][y+1]=1;
+        int cnt=0;
+        scanf("%d %d %d",&M,&N,&K);
+        while(K--){
+            int cab_x,cab_y;
+            scanf("%d %d",&cab_x,&cab_y);
+            land[cab_x][cab_y]=1;
         }
-        dfs(1,1);
+        for(l=0;l<M*N;l++){
+            if(visit[l]==0&&land[l%M][l/M]==1){
+                dfs(l);
+                cnt++;
+            }
+        }
+        for(l=0;l<MAX;l++){
+            land[l%M][l/M]=0;
+            visit[l]=0;
+        } 
+        printf("%d\n",cnt);
     }
 }
