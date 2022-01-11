dc46e2ef1042a65c12a48e2ccec17bee1ccecd65
hatboan <53263563+hatboan@users.noreply.github.com>
2022-01-11 23:05:17 +0900

    Create 7576.cpp
    우선 틀은 잡았고 예제 몇개가 틀려서 손볼 예정

---
+++ 7576.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX 1001
+int box[MAX][MAX];
+int dist[MAX][MAX];
+int biggest=0;
+int m,n;
+
+void bfs(int cur_x,int cur_y){
+    queue<int> qx;
+    queue<int> qy;
+    qx.push(cur_x);
+    qy.push(cur_y);
+    box[cur_x][cur_y]=1;
+    dist[cur_x][cur_y]=0;
+
+    
+    while(!qx.empty()){
+        cur_x=qx.front();
+        cur_y=qy.front();
+        qx.pop();
+        qy.pop();
+
+        int plus_x[4]={1,-1,0,0};
+        int plus_y[4]={0,0,1,-1};
+
+        for(int i=0;i<4;i++){
+            int new_x=cur_x+plus_x[i];
+            int new_y=cur_y+plus_y[i];
+            if(new_x>=0&&new_x<m&&new_y>=0&&new_y<n){
+                if(box[new_x][new_y]==0){
+                    qx.push(new_x);
+                    qy.push(new_y);
+                    box[new_x][new_y]=1;
+                    dist[new_x][new_y]=dist[cur_x][cur_y]+1;
+                    biggest=(biggest>dist[new_x][new_y])?biggest:dist[new_x][new_y];
+                }
+            }
+        }    
+     
+    }
+}
+
+int main(){
+    int checker=0;
+    scanf("%d %d",&m,&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+            scanf("%d",&box[j][i]);
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(box[j][i]==1)
+                bfs(j,i);
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(box[j][i]==0)
+                checker=1;
+        }
+    }
+
+    if(checker==1)
+        printf("-1\n");
+    else{
+        printf("%d\n",biggest);
+    }     
+}
