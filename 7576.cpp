4e26fa7354e188972f4ce215856e66fc73b33d9e
hatboan <53263563+hatboan@users.noreply.github.com>
2022-01-15 11:01:59 +0900

    Update 7576.cpp
    메모리 초과가 발생한다. 어떤 로직을 추가해야 하는 걸까

--- 7576.cpp
+++ 7576.cpp
@@ -2,7 +2,9 @@
 #include <queue>
 using namespace std;
 #define MAX 1001
+int smaller=0;
 int box[MAX][MAX];
+int visit[MAX][MAX][100];
 int dist[MAX][MAX];
 int biggest=0;
 int m,n;
@@ -12,7 +14,8 @@ void bfs(int cur_x,int cur_y){
     queue<int> qy;
     qx.push(cur_x);
     qy.push(cur_y);
-    box[cur_x][cur_y]=1;
+    int smaller_cur=smaller++;
+    visit[cur_x][cur_y][smaller_cur]=1;
     dist[cur_x][cur_y]=0;
 
     
@@ -29,12 +32,13 @@ void bfs(int cur_x,int cur_y){
             int new_x=cur_x+plus_x[i];
             int new_y=cur_y+plus_y[i];
             if(new_x>=0&&new_x<m&&new_y>=0&&new_y<n){
-                if(box[new_x][new_y]==0){
+                if(visit[new_x][new_y][smaller_cur]==0&&box[new_x][new_y]==0){
                     qx.push(new_x);
                     qy.push(new_y);
-                    box[new_x][new_y]=1;
-                    dist[new_x][new_y]=dist[cur_x][cur_y]+1;
-                    biggest=(biggest>dist[new_x][new_y])?biggest:dist[new_x][new_y];
+                    visit[new_x][new_y][smaller_cur]=1;
+                    if(dist[new_x][new_y]!=0)
+                        dist[new_x][new_y]=dist[new_x][new_y]<dist[cur_x][cur_y]+1?dist[new_x][new_y]:dist[cur_x][cur_y]+1;
+                    else dist[new_x][new_y]=dist[cur_x][cur_y]+1;
                 }
             }
         }    
@@ -52,14 +56,18 @@ int main(){
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(box[j][i]==1)
+            if(box[j][i]==1){
                 bfs(j,i);
+            }
         }
     }
 
+
+
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(box[j][i]==0)
+            biggest=biggest>dist[j][i]?biggest:dist[j][i];
+            if(box[j][i]!=1&&box[j][i]!=-1&&dist[j][i]==0)
                 checker=1;
         }
     }
