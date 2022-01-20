fe0749c0efc79b8f1060fd5dc5613c08d50aa14e
hatboan <53263563+hatboan@users.noreply.github.com>
2022-01-20 22:57:18 +0900

    Update 7576.cpp
    다시 짜봤는데도 안 풀리네 얼탱이 없어 ㅋㅋㅋㅋㅋ

--- 7576.cpp
+++ 7576.cpp
@@ -1,85 +1,77 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-#define MAX 1001
-int box[MAX][MAX];
-int dist[MAX][MAX];
-int biggest=0;
+
+#define max 1001
+int land[max][max]={0,};
+int dist[max][max]={0,};
 int m,n;
 
-void bfs(int cur_x,int cur_y){
-    int visit[MAX][MAX]={0,};
-    queue<int> qx;
-    queue<int> qy;
-    qx.push(cur_x);
-    qy.push(cur_y);
-    visit[cur_x][cur_y]=1;
-    dist[cur_x][cur_y]=0;
+void bfs(int x,int y){
+    int visit[max][max]={0,};
 
+    int px[4]={1,-1,0,0};
+    int py[4]={0,0,1,-1};
+    queue <pair<int,int>> q;
+    q.push(make_pair(x,y));
+    visit[x][y]=1;
+    dist[x][y]=1;
 
-    while(!qx.empty()){
-        cur_x=qx.front();
-        cur_y=qy.front();
-        qx.pop();
-        qy.pop();
-
-        int plus_x[4]={1,-1,0,0};
-        int plus_y[4]={0,0,1,-1};
-
-        for(int i=0;i<4;i++){
-            int new_x=cur_x+plus_x[i];
-            int new_y=cur_y+plus_y[i];
-            if(new_x>=0&&new_x<m&&new_y>=0&&new_y<n){
-                if(box[new_x][new_y]==0&&visit[new_x][new_y]==0){
-                    qx.push(new_x);
-                    qy.push(new_y);
-                    visit[new_x][new_y]=1;
-                    if(dist[new_x][new_y]!=0)
-                        dist[new_x][new_y]=dist[new_x][new_y]<dist[cur_x][cur_y]+1?dist[new_x][new_y]:dist[cur_x][cur_y]+1;
-                    else dist[new_x][new_y]=dist[cur_x][cur_y]+1;
-                    
-                    
+    while(!q.empty()){
+        int cx=q.front().first;
+        int cy=q.front().second;
+        q.pop();
+        for(int a=0;a<4;a++){
+            int nx=cx+px[a];
+            int ny=cy+py[a];
+            if(nx>=0&&nx<m&&ny>=0&&ny<n){
+                if(visit[nx][ny]==0&&land[nx][ny]==0){
+                    visit[nx][ny]=1;
+                    q.push(make_pair(nx,ny));
+                    if(dist[nx][ny]==0)
+                        dist[nx][ny]=dist[cx][cy]+1;
+                    else
+                        dist[nx][ny]=dist[nx][ny]<dist[cx][cy]+1?dist[nx][ny]:dist[cx][cy]+1;
                 }
             }
-        }    
+        }
 
     }
+
 }
 
 int main(){
-    int checker=0;
-    queue <int> redtom_x;
-    queue <int> redtom_y;
-    scanf("%d %d",&m,&n);
+    queue <pair<int,int>> ripe_tom;
+    int biggest=0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>m>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            scanf("%d",&box[j][i]);
-            if(box[j][i]==1){
-                redtom_x.push(j);
-                redtom_y.push(i);
-            }
+            cin>>land[j][i];
+            if(land[j][i]==1)
+                ripe_tom.push(make_pair(j,i));
         }
     }
 
-    while(!redtom_x.empty()){
-        int redtom_curx=redtom_x.front();
-        int redtom_cury=redtom_y.front();
-        bfs(redtom_curx,redtom_cury);
-        redtom_x.pop();
-        redtom_y.pop();
+    while(!ripe_tom.empty()){
+        int cur_x=ripe_tom.front().first;
+        int cur_y=ripe_tom.front().second;
+        ripe_tom.pop();
+        bfs(cur_x,cur_y);
     }
 
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             biggest=biggest>dist[j][i]?biggest:dist[j][i];
-            if(box[j][i]!=1&&box[j][i]!=-1&&dist[j][i]==0)
-                checker=1;
+            if(dist[j][i]==0&&land[j][i]!=1&&land[j][i]!=-1){
+                cout<<-1<<"\n";
+                return 0;
+            }
+
         }
     }
 
-    if(checker==1)
-        printf("-1\n");
-    else{
-        printf("%d\n",biggest);
-    }     
+    cout<<biggest-1<<"\n";
 }
