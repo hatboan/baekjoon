6559528fccdfaff06b042b2281885a7c99f25056
hatboan <53263563+hatboan@users.noreply.github.com>
2022-02-04 22:32:23 +0900

    Update 1620.cpp
    풀었다. 이제 만들어진 모듈들을 안 쓰면 안 되는건가 ㅋㅋㅋㅋ

--- 1620.cpp
+++ 1620.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 #define max 100001
 string name[max];
+long pock_hash[max];
 string quest;
 long n,m,l;
+map <string,int> idxs;
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin>>n>>m;
-    for(l=1;l<=n;l++)
+    for(l=1;l<=n;l++){
         cin>>name[l];
+        idxs[name[l]]=l;
+    }
     while(m--){
         cin>>quest;
         long isnum=(long)quest[0];
         if(isnum>47&&isnum<58){
-            long long num=stoll(quest);
+            long num=stoll(quest);
             cout<<name[num]<<"\n";
         }
         else{
-            for(l=1;l<=n;l++){
-                if(quest==name[l]){
-                    cout<<l<<"\n";
-                    break;
-                }
-            }
+            cout<<idxs[quest]<<"\n";
         }
     }
 }
